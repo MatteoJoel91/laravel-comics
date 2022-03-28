a58ffd2892d3e258806f3ccac0503f9ae09027f5
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-03-28 19:06:51 +0200

    push esercizio finito

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,5 +3,22 @@
 @section('pageTitle', 'Comics')
 
 @section('content')
-    main
+<div class="contenitore-main">
+    <div class="button-main-series">
+        <button>CURRENT SERIES</button>
+    </div>
+    <div class="contenuto-main">
+
+        @foreach ($fumetti as $fumetto)
+           <div class="banner">
+                <img src="{{$fumetto['thumb']}}" alt="">
+                <h3>{{$fumetto['series']}}</h3>
+            </div>  
+        @endforeach
+                      
+    </div>
+    <div class="button-main-load">
+        <button>LOAD MORE</button>
+    </div>
+</div>
 @endsection
